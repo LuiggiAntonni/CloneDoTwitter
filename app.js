5f0899024adde697bd467c87d334bb65f2e82b31
LuiggiAntonni <luiggiantonni0129@gmail.com>
2022-03-12 12:25:05 -0300

    adicionando rotas

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express       = require('express');
 const app           = express();
 const port          = 3000;
+const middleware    = require('./middleware')
 
 const server = app.listen(port, () => console.log('Server rodando na porta ' + port))
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res, next) => {
+// Routes
+const loginRoute = require('./routes/loginRoutes')
+
+app.use('/login', loginRoute);
+
+app.get('/', middleware.requireLogin,(req, res, next) => {
 
     var payload = {
         pageTitle: "Home"
