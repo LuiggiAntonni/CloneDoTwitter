6c5243c5216d4359a78c7f86938092d7403d6916
LuiggiAntonni <luiggiantonni0129@gmail.com>
2022-03-14 10:37:15 -0300

    Likes

--- public/js/common.js
+++ public/js/common.js
@@ -32,12 +32,47 @@ $("#submitPostButton").click(() => {
 
 })
 
+$(document).on("click", ".likeButton", (event) => {
+    var button = $(event.target);
+    var postId = getPostIdFromElement(button);
+    
+    if(postId === undefined) {
+        return;
+    }
+
+    $.ajax({
+        url: `/api/posts/${postId}/like`,
+        type: "PUT",
+        success: (postData) => {
+            
+            button.find("span").text(postData.likes.length || "");
+
+        }
+    })
+})
+
+function getPostIdFromElement(element) {
+    var isRoot = element.hasClass("post");
+    var rootElement = isRoot ? element : element.closest(".post");
+    var postId = rootElement.data().id;
+
+    if(postId === undefined) return console.log("Post não definido");
+
+    return postId;
+}   
+
 function createPostHtml(postData) {
 
     var postedBy = postData.postedBy;
+
+    if(postedBy._id === undefined) {
+        return 0
+    }
+
+
     var displayName = postedBy.firstName + " " + postedBy.lastName;
-    var timeStamp = postData.createdAt;
-    return `<div class="post">
+    var timeStamp = timeDifference(new Date(), new Date(postData.createdAt));
+    return `<div class="post" data-id="${postData._id}">
                 
                 <div class="mainContentContainer">
                     <div class="userImageContainer">
@@ -45,7 +80,7 @@ function createPostHtml(postData) {
                     </div>
                     <div class="postContentContainer">
                         <div class="header">
-                            <a href="/profile/${postedBy.username}">${displayName}</a>
+                            <a href="/profile/${postedBy.username}" class="displayName">${displayName}</a>
                             <span class="username">@${postedBy.username}</span>
                             <span class="date">${timeStamp}</span>
                         </div>
@@ -53,8 +88,59 @@ function createPostHtml(postData) {
                             <span>${postData.content}</span>
                         </div>
                         <div class="postFooter">
+                            <div class="postButtonContainer">
+                                <button>
+                                    <i class="far fa-comment"></i>
+                                </button>
+                            </div>
+                            <div class="postButtonContainer">
+                                <button>
+                                    <i class="fas fa-retweet"></i>
+                                </button>
+                            </div>
+                            <div class="postButtonContainer">
+                                <button class="likeButton">
+                                    <i class="far fa-heart"></i>
+                                    <span>${postData.likes.length || ""}</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>`;
+}
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' segundos atrás';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutos atrás';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' horas atrás';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' dias atrás';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' meses antes';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' anos atrás';   
+    }
 }
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,16 @@ const jwt           = require("jsonwebtoken");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res, next) => {
+    Post.find()
+    .populate("postedBy")
+    .sort({ "createdAt" : -1 })
+    .then((results) => {
+        res.status(200).send(results);
+    })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
 });
 
 router.post('/', async (req, res, next) => {
@@ -34,6 +44,42 @@ router.post('/', async (req, res, next) => {
         console.log(err);
         return res.sendStatus(400);
     })
+});
+
+router.put('/:id/like', async (req, res, next) => {
+    
+    var postId = req.params.id;
+    var user = jwt.decode(req.session.user).user
+    var userId = user._id;
+    
+    var isLiked = user.likes && user.likes.includes(postId); 
+
+    var option = isLiked ? "$pull" : "$addToSet"
+    
+    // Insert user like
+    user = await User.findByIdAndUpdate(userId, { [option]: {likes: postId} }, { new: true })
+    .catch(error => {
+        console.log(error);
+        return res.sendStatus(400);
+    })
+
+    let token = jwt.sign({
+        user: user
+    }, process.env.JWT_KEY, {
+        expiresIn: "1h"
+    });
+    req.session.user = token
+
+
+    // Insert post like
+    var post = await Post.findByIdAndUpdate(postId, { [option]: {likes: userId} }, { new: true })
+    .catch(error => {
+        console.log(error);
+        return res.sendStatus(400);
+    })
+
+
+    res.status(200).send(post);
 });
 
 module.exports = router;
